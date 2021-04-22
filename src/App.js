778b22877ab10defd31961d61de90ed3d0b2c129
KarrynS <karrynsaw@gmail.com>
2021-04-22 14:34:46 +1000

    fixin eact path on "/"

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
       <>
         <Navbar />
         <Switch >
-        <Route exact path="/" component={Home} />
-        <Route exact path="/projects" component={Projects} />
-        <Route exact path="/contact" component={Contact} /> 
+        <Route exact path={["/", ]} component={Home} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/contact" component={Contact} /> 
         </Switch>
         <Footer />
       </>
