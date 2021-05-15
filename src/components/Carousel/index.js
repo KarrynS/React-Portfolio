b3379a9b05aeb509f771ff625c1c6d2a596f1d26
KarrynS <karrynsaw@gmail.com>
2021-05-15 21:48:03 +1000

    changing media queries for carousel

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -20,28 +20,28 @@ const Carousel = () => {
             <div className="carousel-inner">
                 <div className="carousel-item active" data-interval="4000">
                     <img src={ImgOne} className="d-block w-100 img-fluid" alt="laptop on wooden desk" />
-                    <div className="carousel-caption caption-large d-none d-sm-block">
+                    <div className="carousel-caption caption-large d-sm-block">
                         <h5 className="carouselTitle">Full Stack Software Developer</h5>
                         <p className="carouselPara">with a passion for learning and eye for detail</p>
                     </div>
                 </div>
             <div className="carousel-item" data-interval="4000">
                 <img src={ImgTwo} className="d-block w-100 img-fluid" alt="typewriter on old desk"/>
-                <div className="carousel-caption d-none d-sm-block ">
+                <div className="carousel-caption d-sm-block ">
                     <h5 className="carouselTitle">Think outside the box</h5>
                     <p className="carouselPara">Never afraid to return back to basics and push boundaries</p>
                 </div>
             </div>
             <div className="carousel-item" data-interval="4000">
-                <img src={ImgThree} className="d-block w-100 img-fluid" alt="peope canoeing"/>
-                <div className="carousel-caption  d-none d-sm-block">
+                <img src={ImgThree} className="d-block w-100 img-fluid" alt="people canoeing"/>
+                <div className="carousel-caption d-sm-block">
                     <h5 className="carouselTitle" id="collabTitle">Collaboration</h5>
                     <p className="carouselPara" id="collab">Always ready to listen and keen to begin solving problems</p>
                 </div>
             </div>
             <div className="carousel-item" data-interval="4000">
-                <img src={ImgFour} className="d-block w-100 img-fluid" alt="phot of two pairs of shoes"/>
-                <div className="carousel-caption d-none d-sm-block ">
+                <img src={ImgFour} className="d-block w-100 img-fluid" alt="photo of pair of shoes"/>
+                <div className="carousel-caption d-sm-block ">
                     <h5 className="carouselTitle">Drive</h5>
                     <p className="carouselPara">Driven by a passion to know better with each iteration</p>
                 </div>
